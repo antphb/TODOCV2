8b0ad40b74349dba0c6886c3bfb4c34846cd365d
antphb <thanhnd2462245@gmail.com>
2023-03-09 23:27:18 +0700

    demo2

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings, static
+
 urlpatterns = [
     path("",views.home,name="home"),
     path("CV1/",views.CV1,name="CV1"),
@@ -8,3 +10,6 @@ urlpatterns = [
     path("ToDo1/",views.ToDo1,name="ToDo1"),
     path("ToDo2/",views.ToDo2,name="ToDo2"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
